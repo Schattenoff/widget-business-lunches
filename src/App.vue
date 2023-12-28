7d67f167c4cbb02a0d19ea10d778b8fb57b37e73
Thomas <t.kudin@generals-soft.com>
2023-12-28 14:40:55 +0300

    upload app via vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>Hello, world!</div>
+    <div v-for="(lunch, index) in businessLunchList.data" :key="index">
+      <h1>{{ lunch.name }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import businessLunchList from "./mockData/business-lunches.json";
+
+export default {
+  data() {
+    return {
+      businessLunchList,
+    };
+  },
+};
+</script>
